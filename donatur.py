b8f7d680701ebf9b2ddf1d12bd6ca0c7337b5b04
kunfayz6-lab <kunfayz6@gmail.com>
2026-05-27 10:37:57 +0700

    Update donatur.py

--- donatur.py
+++ donatur.py
@@ -38,7 +38,8 @@ while True:
             jumlah = int(input("\033[37mJumlah Uang Sumbangan (Rp): "))
             no_rek = int(input("\033[37mno_rek ():  "))
 
-            
+        print("\033[33m\n--- a/ ---")
+        nama = input("\033[33ma/n : ")       
             # Simpan data ke dalam list
             daftar_donasi.append({
                 "nama": nama,
