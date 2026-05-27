3f0292024736bd0857dff3304d220d75cc0f9ebc
kunfayz6-lab <kunfayz6@gmail.com>
2026-05-27 08:40:17 +0700

    Update donatur.py

--- donatur.py
+++ donatur.py
@@ -73,7 +73,7 @@ while True:
             print(f"Rata-rata Sumbangan  : Rp. {rata_rata:,.2f}")
 
     elif pilihan == '4':
-        print("\nTerima kasih telah menggunakan program ini. Program selesai!")
+        print("\n\033[33mTERIMA KASIH SUDAH MEMPERCAYAKAN AMANAHNYA MELALUI BUMI SYAM & 313!\033[37m Jajalallahu khairan....!!\033[0m")
         break
     else:
         print("Pilihan tidak valid. Silakan masukkan angka 1 hingga 4.")
